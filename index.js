7d0933948de1a8c9182062d3a34b48d0cdc2c486
Sapotong <new.m.tah@gmail.com>
2022-12-22 14:08:29 +0700

    Menambah info OS

--- index.js
+++ index.js
@@ -7,3 +7,5 @@ si.mem().then(data => console.log('mem', data));
 si.fsSize().then(data => console.log('fs', data));
 
 si.diskLayout().then(data => console.log('disk', data));
+
+si.osInfo().then(data => console.log('os', data));
